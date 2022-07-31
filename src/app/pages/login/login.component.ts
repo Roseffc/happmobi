6a4792376f4ef2de92f0bc12f92a68bb16b9b980
Roseffc <roseffc@gmail.com>
2022-07-30 22:12:48 -0300

    Configurado validação formulário da página login e criado rota home

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin!:FormGroup
+
+  constructor(private formBuilder:FormBuilder,private router:Router) { }
 
   ngOnInit(): void {
+    this.setFormGroup()
+  }
+
+  setFormGroup() {
+    this.formLogin = this.formBuilder.group({
+      username:[null, Validators.required],
+      password:[null, Validators.required],
+    })
+  }
+
+  onSubmit() {
+    this.router.navigate(['home']);
   }
 
 }
